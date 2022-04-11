fedc985d6c3acadefa1e591f3aa778177fe06155
CharlieRojas <crc_185@yahoo.com>
2022-04-11 09:49:24 -0500

    commit tree c1a5331600d1dcda6aa7a334cb39ff0efbda7af9

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+
+const blurHandler = () => console.log('Hey! Eyes on me!');
+const focusHandler = () => console.log('Good!')
+
+function EyesOnMe() {
+  return (
+    <div>
+        <button onFocus={focusHandler} onBlur={blurHandler}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+import React from 'react'
+
+const handleChange = () =>{
+    console.log('Entering password...')
+}
+
+function Keypad() {
+  return (
+      <div>
+    <input type = "password" onChange={handleChange}></input>
+    </div>
+  )
+}
+
+export default Keypad
